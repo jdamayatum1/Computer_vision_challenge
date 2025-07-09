fb91ad824c68c1a8ee5108a2385f8bc3c8fbaec5
Dinis Carmona <diniscarmona@gmail.com>
2025-07-09 11:18:15 +0200

    Manual input of control points

--- image_alignment.m
+++ image_alignment.m
@@ -1,7 +1,26 @@
-% Load the images
-fixedImage = imread('12_1990.jpg'); % Replace with your fixed image
-movingImage = imread('12_2020.jpg');    % Replace with your moving image
-% For these two, image should be in the same folder
+% % Load the images
+% fixedImage = imread('12_2000.jpg'); % Replace with your fixed image
+% movingImage = imread('12_2006.jpg');    % Replace with your moving image
+% % For these two, image should be in the same folder
+
+
+% Prompt user to select the fixed image
+[fixedImageFile, fixedImagePath] = uigetfile({'*.jpg;*.png;*.tif', 'Image Files (*.jpg, *.png, *.tif)'}, 'Select Fixed Image');
+if isequal(fixedImageFile, 0)
+    disp('User canceled the selection.');
+    return; % Exit if no file is selected
+end
+fixedImage = imread(fullfile(fixedImagePath, fixedImageFile));
+
+% Prompt user to select the moving image
+[movingImageFile, movingImagePath] = uigetfile({'*.jpg;*.png;*.tif', 'Image Files (*.jpg, *.png, *.tif)'}, 'Select Moving Image');
+if isequal(movingImageFile, 0)
+    disp('User canceled the selection.');
+    return; % Exit if no file is selected
+end
+movingImage = imread(fullfile(movingImagePath, movingImageFile));
+
+
 
 % Convert to grayscale if necessary
 if size(fixedImage, 3) == 3
@@ -25,7 +44,14 @@ matchedPointsFixed = validPointsFixed(indexPairs(:, 1), :);
 matchedPointsMoving = validPointsMoving(indexPairs(:, 2), :);
 
 % Estimate geometric transformation
-[tform, inlierIdx] = estimateGeometricTransform(matchedPointsMoving, matchedPointsFixed, 'affine', 'MaxNumTrials', 5000, 'Confidence', 90);
+if isempty(indexPairs)
+    disp('Automatic feature matching failed. Please select control points manually.');
+    [movingPoints, fixedPoints] = cpselect(movingImage, fixedImage, 'Wait', true);
+    % Estimate transformation using selected control points
+    [tform, inlierIdx] = estimateGeometricTransform(matchedPointsMoving, matchedPointsFixed, 'rigid');
+else
+    [tform, inlierIdx] = estimateGeometricTransform(matchedPointsMoving, matchedPointsFixed, 'affine', 'MaxNumTrials', 5000, 'Confidence', 90);
+end
 
 % Check condition number
 conditionNumber = cond(tform.T);
